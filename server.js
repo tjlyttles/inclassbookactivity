df38b412c6ed694f54e98be2da312808b0fabace
tjlyttles <tjlyttles84@gmail.com>
2019-07-08 21:29:28 -0700

    added db url

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://dbuser:dbpassword@ds249267.mlab.com:49267/heroku_9d2ljxkk"
+);
 
 // Start the API server
 app.listen(PORT, function() {
